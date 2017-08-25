30da0f0756e64bfe648702e172f43a467ea81c0b
Остап Бендер <ostapbender@MacBook.local>
2017-08-25 11:51:37 +0400

    default request not working

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,6 @@
+<form action="" method="POST">
+    {{ csrf_field() }}
+    <textarea name="areatest" rows="3"></textarea><br>
+    <input name="sep" type="text" placeholder="testing" value="">
+    <input type="submit">
+</form>
